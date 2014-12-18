734113a7588deefbad88c41811deed83c1afd80a
Craig D'Amelio <craig.damelio@gmail.com>
2014-12-18 09:41:41 -0500

    trying to add a snapchat service provider

--- src/index.php
+++ src/index.php
@@ -3,5 +3,8 @@
 require_once("vendor/autoload.php");
 
 $app = new \Cilex\Application('SnapchatStoryLogger');
+
+$app->register(new \Cilex\Provider\ConfigServiceProvider(), array('config.path' => ''));
+
 $app->command(new \acdameli\Snapchat\Commands\StoryLoggerCommand());
 $app->run();
